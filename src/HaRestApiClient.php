e9b0248903932dc111c3159fe58d1977c09419ba
IndexZer0 <24657691+IndexZer0@users.noreply.github.com>
2024-02-22 18:35:08 +0000

    - Change HaRestApiClient to not pass functions to handleRequest. - Added method comments. - Added general comments. - General cleanup.

--- src/HaRestApiClient.php
+++ src/HaRestApiClient.php
@@ -8,14 +8,18 @@ use DateTimeInterface;
 use GuzzleHttp\Client as GuzzleClient;
 use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\RequestOptions;
 use JsonException;
+use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Throwable;
 
 class HaRestApiClient
 {
     public readonly GuzzleClient $guzzleClient;
+
+    private static string $dateFormat = 'Y-m-d\Th:m:sP';
 
     public function __construct(
         private string        $bearerToken,
@@ -25,6 +29,9 @@ class HaRestApiClient
         $this->initGuzzleClient();
     }
 
+    /*
+     * Init guzzle client.
+     */
     protected function initGuzzleClient(): void
     {
         $this->guzzleClient = new GuzzleClient([
@@ -37,34 +44,44 @@ class HaRestApiClient
         ]);
     }
 
+    /*
+     * Returns a message if the API is up and running.
+     */
     public function status(): array
     {
-        return $this->handleRequest(function () {
-            return $this->guzzleClient->get('');
-        });
+        return $this->handleRequest(new Request('GET', ''));
     }
 
+    /*
+     * Returns the current configuration.
+     */
     public function config(): array
     {
-        return $this->handleRequest(function () {
-            return $this->guzzleClient->get('config');
-        });
+        return $this->handleRequest(new Request('GET', 'config'));
     }
 
+    /*
+     * Returns an array of event objects.
+     * Each event object contains event name and listener count.
+     */
     public function events(): array
     {
-        return $this->handleRequest(function () {
-            return $this->guzzleClient->get('events');
-        });
+        return $this->handleRequest(new Request('GET', 'events'));
     }
 
+    /**
+     * Returns an array of service objects.
+     * Each object contains the domain and which services it contains.
+     */
     public function services(): array
     {
-        return $this->handleRequest(function () {
-            return $this->guzzleClient->get("services");
-        });
+        return $this->handleRequest(new Request('GET', 'services'));
     }
 
+    /*
+     * Returns an array of state changes in the past.
+     * Each object contains further details for the entities.
+     */
     public function history(
         array              $entityIds,
         ?DateTimeInterface $startTime = null,
@@ -85,19 +102,15 @@ class HaRestApiClient
 
         $path = "history/period";
 
-        $dateFormat = 'Y-m-d\Th:m:sP';
-
         if ($startTime !== null) {
-            $path .= '/' . $startTime->format($dateFormat);
+            $path .= '/' . $startTime->format(self::$dateFormat);
         }
 
         $booleanQueryParams = array_filter([
             'minimal_response'         => $minimalResponse,
             'no_attributes'            => $noAttributes,
             'significant_changes_only' => $significantChangesOnly,
-        ], function ($value) {
-            return $value === true;
-        });
+        ], fn ($value) => $value);
 
         $queryParams = [
             'filter_entity_id' => join(',', $entityIds),
@@ -105,16 +118,17 @@ class HaRestApiClient
         ];
 
         if ($endTime !== null) {
-            $queryParams['end_time'] = $endTime->format($dateFormat);
+            $queryParams['end_time'] = $endTime->format(self::$dateFormat);
         }
 
-        return $this->handleRequest(function () use ($path, $queryParams) {
-            return $this->guzzleClient->get($path, [
-                'query' => $queryParams
-            ]);
-        });
+        return $this->handleRequest(new Request('GET', $path), [
+            'query' => $queryParams
+        ]);
     }
 
+    /*
+     * Returns an array of logbook entries.
+     */
     public function logbook(
         ?string            $entityId = null,
         ?DateTimeInterface $startTime = null,
@@ -122,10 +136,8 @@ class HaRestApiClient
     ): array {
         $path = "logbook";
 
-        $dateFormat = 'Y-m-d\Th:m:sP';
-
         if ($startTime !== null) {
-            $path .= '/' . $startTime->format($dateFormat);
+            $path .= '/' . $startTime->format(self::$dateFormat);
         }
 
         $queryParams = [];
@@ -135,137 +147,150 @@ class HaRestApiClient
         }
 
         if ($endTime !== null) {
-            $queryParams['end_time'] = $endTime->format($dateFormat);
+            $queryParams['end_time'] = $endTime->format(self::$dateFormat);
         }
 
-        return $this->handleRequest(function () use ($path, $queryParams) {
-            return $this->guzzleClient->get($path, [
-                'query' => $queryParams
-            ]);
-        });
+        return $this->handleRequest(new Request('GET', $path), [
+            'query' => $queryParams
+        ]);
     }
 
+    /*
+     * Returns an array of state objects.
+     * Each state has the following attributes: entity_id, state, last_changed and attributes.
+     */
     public function states(): array
     {
-        return $this->handleRequest(function () {
-            return $this->guzzleClient->get('states');
-        });
+        return $this->handleRequest(new Request('GET', 'states'));
     }
 
+    /*
+     * Returns a state object for specified entity_id.
+     * Throws exception if entity not found.
+     */
     public function state(string $entityId): array
     {
-        return $this->handleRequest(function () use ($entityId) {
-            return $this->guzzleClient->get("states/{$entityId}");
-        });
+        return $this->handleRequest(new Request('GET', "states/{$entityId}"));
     }
 
+    /*
+     * Retrieve all errors logged during the current session of Home Assistant.
+     */
     public function errorLog(): array
     {
-        return $this->handleRequest(function () {
-            return $this->guzzleClient->get('error_log');
-        });
+        return $this->handleRequest(new Request('GET', "error_log"));
     }
 
-    // TODO
+    /*
+     * Returns the data (image) from the specified camera entity_id.
+     * // TODO implement
+     */
     /*public function camera(string $entityId): array
     {
+        throw new \Exception('Not implemented');
+
         // TODO get param (time).
 
-        return $this->handleRequest(function () use ($entityId) {
-            return $this->guzzleClient->get("camera_proxy/{$entityId}");
-        });
+        return $this->handleRequest(new Request('GET', "camera_proxy/{$entityId}"));
     }*/
 
+    /*
+     * Returns the list of calendar entities.
+     */
     public function calendars(): array
     {
-        return $this->handleRequest(function () {
-            return $this->guzzleClient->get('calendars');
-        });
+        return $this->handleRequest(new Request('GET', "calendars"));
     }
 
+    /*
+     * Returns the list of calendar events for the specified calendar entity_id between the start and end times (exclusive).
+     */
     public function calendarEvents(
         string            $entityId,
         DateTimeInterface $start,
         DateTimeInterface $end,
     ): array {
-        $dateFormat = 'Y-m-d\Th:m:sP';
-
-        $queryParams = [
-            'start' => $start->format($dateFormat),
-            'end'   => $end->format($dateFormat)
-        ];
-
-        return $this->handleRequest(function () use ($entityId, $queryParams) {
-            return $this->guzzleClient->get("calendars/{$entityId}", [
-                'query' => $queryParams
-            ]);
-        });
+        return $this->handleRequest(new Request('GET', "calendars/{$entityId}"), [
+            'query' => [
+                'start' => $start->format(self::$dateFormat),
+                'end'   => $end->format(self::$dateFormat)
+            ]
+        ]);
     }
 
+    /*
+     * Updates or creates a state. You can create any state that you want, it does not have to be backed by an entity in Home Assistant.
+     */
     public function updateState(
         string $entityId,
         string $state,
         ?array $attributes
     ): array {
-
         $data = ['state' => $state,];
 
         if ($attributes !== null) {
             $data['attributes'] = $attributes;
         }
 
-        return $this->handleRequest(function () use ($entityId, $data) {
-            return $this->guzzleClient->post("states/{$entityId}", [
-                RequestOptions::JSON => $data,
-            ]);
-        });
+        return $this->handleRequest(new Request('POST', "states/{$entityId}"), [
+            RequestOptions::JSON => $data,
+        ]);
     }
 
+    /*
+     * Fires an event with event_type.
+     * Please be mindful of the data structure as documented on the Data Science portal.
+     * https://data.home-assistant.io/docs/events/#database-table
+     */
     public function fireEvent(
         string $eventType,
         ?array $eventData = null
     ): array {
-        return $this->handleRequest(function () use ($eventType, $eventData) {
-            return $this->guzzleClient->post("events/{$eventType}", [
-                RequestOptions::JSON => $eventData,
-            ]);
-        });
+        return $this->handleRequest(new Request('POST', "events/{$eventType}"), [
+            RequestOptions::JSON => $eventData,
+        ]);
     }
 
+    /*
+     * Calls a service within a specific domain. Will return when the service has been executed.
+     */
     public function callService(string $domain, string $service, array $data): array
     {
-        $url = "services/{$domain}/{$service}";
-
-        return $this->handleRequest(function () use ($url, $data) {
-            return $this->guzzleClient->post($url, [
-                RequestOptions::JSON => $data,
-            ]);
-        });
+        return $this->handleRequest(new Request('POST', "services/{$domain}/{$service}"), [
+            RequestOptions::JSON => $data,
+        ]);
     }
 
+    /*
+     * Render a Home Assistant template.
+     * See template docs for more information.
+     * https://www.home-assistant.io/docs/configuration/templating
+     */
     public function renderTemplate(string $template): array
     {
-        return $this->handleRequest(function () use ($template) {
-            return $this->guzzleClient->post('template', [
-                RequestOptions::JSON => ['template' => $template],
-            ]);
-        });
+        return $this->handleRequest(new Request('POST', 'template'), [
+            RequestOptions::JSON => ['template' => $template],
+        ]);
     }
 
+    /*
+     * Trigger a check of configuration.yaml.
+     * Needs config integration enabled.
+     */
     public function checkConfig(): array
     {
-        return $this->handleRequest(function () {
-            return $this->guzzleClient->post('config/core/check_config');
-        });
+        return $this->handleRequest(new Request('POST', 'config/core/check_config'));
     }
 
+    /*
+     * Handle an intent.
+     * You must add `intent:` to your configuration.yaml.
+     */
     public function handleIntent(array $data): array
     {
-        return $this->handleRequest(function () use ($data) {
-            return $this->guzzleClient->post('intent/handle', [
-                 RequestOptions::JSON => $data,
-            ]);
-        });
+        return $this->handleRequest(new Request('POST', 'intent/handle'), [
+            RequestOptions::JSON => $data,
+        ]);
     }
 
     /**
@@ -274,11 +299,13 @@ class HaRestApiClient
      * ---------------------------------------------------------------------------------
      */
 
-    private function handleRequest(callable $callable): array
+    /*
+     * Send request and handle responses.
+     */
+    private function handleRequest(RequestInterface $request, array $options = []): array
     {
         try {
-            /** @var ResponseInterface $response */
-            $response = $callable();
+            $response = $this->guzzleClient->send($request, $options);
         } catch (ClientException $ce) {
             throw new HaException($ce->getResponse()->getBody()->getContents(), previous: $ce);
         } catch (Throwable $t) {
@@ -287,25 +314,37 @@ class HaRestApiClient
 
         $responseBodyContent = $response->getBody()->getContents();
 
-        $contentType = 'application/json';
-        if ($response->hasHeader('Content-Type')) {
-            $contentType = $response->getHeader('Content-Type')[0];
-        }
+        $responseContentType = $this->getContentTypeFromResponse($response) ?? 'application/json';
 
-        if ($contentType === 'application/json') {
+        if ($responseContentType === 'application/json') {
             try {
                 $json = json_decode($responseBodyContent, true, flags: JSON_THROW_ON_ERROR);
+
+                // This is a failsafe for if the home assistant json response is not an array when decoded
+                // For example if $responseBodyContent = 'null';
+                // Not seen this scenario in the wild but handling this json decode case anyway.
                 if (!is_array($json)) {
                     return [$json];
                 }
+
                 return $json;
             } catch (JsonException $je) {
+                // This should never happen.
+                // If it does, it means home assistant is returning invalid json with application/json Content-Type header.
                 throw new HaException('Invalid JSON Response.', previous: $je);
             }
         }
 
+        // Some responses come back with Content-Type header of text/plain.
+        // Such as errorLog and renderTemplate.
+        // So lets just wrap in an array to satisfy return type and keep api consistent.
         return [
             'response' => $responseBodyContent
         ];
+    }
+
+    private function getContentTypeFromResponse(ResponseInterface $response): ?string
+    {
+        return $response->hasHeader('Content-Type') ? $response->getHeader('Content-Type')[0] : null;
     }
 }
